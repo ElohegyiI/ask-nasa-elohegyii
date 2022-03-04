7be2c48b37deb034176fc8de14b7b0750b2e0cef
ElohegyiI <roxnadz78@gmail.com>
2022-03-04 16:46:23 +0100

    karvolt

--- frontend/script.js
+++ frontend/script.js
@@ -1,55 +1,60 @@
-function Nasa(date, explanation, title, hdurl) {
-
-    this.date = date;
-    this.explanation = explanation;
-    this.title = title;
-    this.hdurl = hdurl;
-}  
-
-
-
-const loadEvent = async _ => {
+function loadEvent() {
     
-    const nasaCard = (nasa) => {
+    
+    /*function Nasa(date, explanation, title, hdurl) {
+        
+        this.date = date;
+        this.explanation = explanation;
+        this.title = title;
+        this.hdurl = hdurl;
+    }*/ 
+    
+    const rootElement = document.getElementById('root')
+    
+    const sect = document.createElement('section')
+        sect.setAttribute('id', 'Nasa-Cards')
+        document.getElementById('root').appendChild(sect)
+
+    /*const division = document.createElement('div')
+        division.className = 'Nasa'
+        document.getElementById('root').appendChild(sect).appendChild(division)*/
+
+
+
             
-        return `
+        /*return `
         <div class="card">
-            <img>${nasa.hdurl}</img
-            <h2>${nasa.title}</h2>
-            <p>${nasa.explanation}</p>
-            <p>${nasa.date}</p>
+            <img>${Nasa.hdurl}</img
+            <h2>${Nasa.title}</h2>
+            <p>${Nasa.explanation}</p>
+            <p>${Nasa.date}</p>
         </div>
-        `
-    }
+        `*/
     
-    const nasaCards = (sect) => {
     
-        return`
-            <section class="Nasa-Cards>${sect}</section>
-        `
-    }
+    
+    
+        
+
+        
+    
         
     
     //Get data
 
-    const nasaApiKey = 'd8rG7d4dYIisXPSU8KBH17ipMYR7pPGj1tjRgNqD';
+    /*const nasaApiKey = 'd8rG7d4dYIisXPSU8KBH17ipMYR7pPGj1tjRgNqD';
     
     const requestedDate = "2022-02-01";
     
     const nasaPic = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${nasaApiKey}&date=${requestedDate}`);
 
     
-    const nasaArr = await nasaPic.json();
+    const nasaArr = await nasaPic.json();*/
     
     //console.log(nasaArr);
 
-    const rootElement = document.getElementById('root')
 
-    const division = document.createElement('div')
-        division.className = 'Nasa'
-        document.getElementById('root').appendChild(division)
-
-    rootElement.insertAdjacentHTML('beforeend', nasaCards())
+    //rootElement.insertAdjacentHTML('beforeend', nasaCards())
 
 
     //Process data
