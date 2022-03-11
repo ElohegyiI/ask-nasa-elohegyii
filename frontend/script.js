5baab24a38cd8ba081c9d3e360775888d30ef651
ElohegyiI <roxnadz78@gmail.com>
2022-03-11 16:13:30 +0100

    update

--- frontend/script.js
+++ frontend/script.js
@@ -28,9 +28,9 @@ function loadEvent() {
         <p id="explan"></p>
         `);
 
-    const divisionDate = document.createElement('div')
+    /*const divisionDate = document.createElement('div')
     divisionDate.className = 'Date'
-    document.getElementById('root').appendChild(sect).appendChild(divisionDate)
+    document.getElementById('root').appendChild(sect).appendChild(divisionDate)*/
 
     const divisionDatePick = document.createElement('div')
     divisionDatePick.className = 'Date-Pick'
@@ -44,20 +44,20 @@ function loadEvent() {
     </form>
     `);
 
-    const divisionMedia = document.createElement('div')
+    /*const divisionMedia = document.createElement('div')
     divisionMedia.className = 'Media'
-    document.getElementById('root').appendChild(sect).appendChild(divisionMedia)
+    document.getElementById('root').appendChild(sect).appendChild(divisionMedia)*/
 
 
     const nasaApiKey = 'd8rG7d4dYIisXPSU8KBH17ipMYR7pPGj1tjRgNqD';
 
     const url = 'https://api.nasa.gov/planetary/apod?api_key=';
 
-    function fetchData(){
-        try{
-          fetch(url+nasaApiKey)
-          .then(response=>response.json())
-          .then(json=>{
+    function fetchData() {
+        try {
+          fetch(url + nasaApiKey)
+          .then(response => response.json())
+          .then(json => {
             console.log(json)
             display(json)
           })
@@ -81,44 +81,48 @@ function loadEvent() {
 
         info.innerHTML = adat.explanation
 
-        const imageSection =`<a id="hdimg" href="" target="-blank">
+        const imageSection = `<a id="hdimg" href="" target="-blank">
             <div class="image-div">
                 <img id="image_of_the_day" src="" alt="image-by-nasa">
             </div>
             </a>`
             ;
 
-        const videoSection=`<div class="video-div"> 
+        const videoSection = `<div class="video-div"> 
                 <iframe id="videoLink" src=""frameborder="0">
                 </iframe>
             </div>`
             ;
 
-        if(adat.media_type == "video"){
+        if (adat.media_type = "video") {
+
             mediaSection.innerHTML = videoSection;
-            document.getElementById("videoLink").src=adat.url;
+            document.getElementById("videoLink").src = adat.url;
        
         } else {
+
             mediaSection.innerHTML = imageSection;
-            document.getElementById("hdimg").href=adat.hdurl;
-            document.getElementById("image_of_the_day").src=adat.url;
+            document.getElementById("hdimg").href = adat.hdurl;
+            document.getElementById("image_of_the_day").src = adat.url;
         }
-
-
-            const dateInput = document.querySelector(".Date-Pick");
+        
+        
+        const dateInput = document.querySelector("#datepicker");
+        
+        let newDate = "&date=" + dateInput.value + "&";
+        
+        fetch(url + nasaApiKey + newDate)
+        
+        const currentDate = new Date().toISOString().slice(0, 10);
+        dateInput.max = currentDate;
+        dateInput.min = "2000-01-01";
+        
             
-            let newDate = "&date=" + dateInput.value + "&";
-
-            fetch(url + nasaApiKey + newDate)
-
-            const currentDate = new Date().toISOString().slice(0, 10);
-                dateInput.max = currentDate;
-                dateInput.min = "2000-01-01";
-
-            
-            dateInput.addEventListener('change',(e) => {
-                 e.preventDefault();
-                 display().onload;
+                dateInput.addEventListener('change',(e) => {
+                e.preventDefault();
+                display().onload;  
+                
+                
                 });
             
     }
